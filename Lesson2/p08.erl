4b6a03ed3dba4f11ec7cc24b45c60d9247da3901
Romaniuk Vadim <lavadimka1997@gmail.com>
2018-03-09 21:42:53 +0200

    Work

--- Lesson2/p08.erl
+++ Lesson2/p08.erl
@@ -8,32 +8,23 @@
 %% 1> p08:compress([a,a,a,a,b,c,c,a,a,d,e,e,e,e]). %%
 %% [a,b,c,a,d,e]                                   %%
 
-%%
+reverse(L) ->
+    reverse(L,[]).
+reverse([],Tail) ->
+    Tail;
+reverse([H|T], Tail) ->
+    reverse(T,[H|Tail]).
 
-but_last([_,_]=L) ->
-    L;
-but_last([_H|T]) ->
-    but_last(T).
 
-%%
 
 compress(List) ->
     compress(List,[]). %% Interface %%
 
-compress([H|_T]=[H,H|Rest],ListWithoutDub=[H|_]) ->
-    compress(Rest,ListWithoutDub); %% If first element of ListWithoutDublicate equal first element from raw list, don't add this element %%
-
-%%compress([H|_],but_last(ListWithoutDub)=[H,H|_]) ->
-%%    ListWithoutDub; %%
-
-compress([H|T],ListWithoutDub =[H|_]) ->
-    compress(T,ListWithoutDub); %%
-
-compress([H|_T]=[H,H|Rest],ListWithoutDub) ->
-    compress(Rest,ListWithoutDub++[H]); %% If raw list have the same second element, send to function the rest after two elements %%
+compress([H|T],ListWithoutDub=[H|_]) ->
+    compress(T,ListWithoutDub); %% If first raw list element like first element in ListWithoutdublicate %%
 
 compress([H|T],ListWithoutDub) ->
-    compress(T,ListWithoutDub++[H]); %%
+    compress(T,[H|ListWithoutDub]); %% 
 
 compress([],ListWithoutDub) ->
-    ListWithoutDub. %% When list is empty. Return LastWithoutDublicate. %%
+    reverse(ListWithoutDub). %% When list is empty. Return LastWithoutDublicate. %%
