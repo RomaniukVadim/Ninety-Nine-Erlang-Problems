fb6a09cf7f691ca53264d4e5aa4c78b4eeda8a85
Romaniuk Vadim <lavadimka1997@gmail.com>
2018-03-10 20:47:37 +0200

    BS02. Test passed.

--- Lesson3/bs02.erl
+++ Lesson3/bs02.erl
@@ -1,6 +1,6 @@
 -module(bs02).
 
--export(words/1).
+-export([words/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 %%
@@ -12,10 +12,33 @@
 %% [<<"Text">>, <<"with">>, <<"four">>, <<"words">>] %%
 %%
 
+del_space(<<$\s, Rest/binary>>) ->
+    del_space(Rest);
+del_space(Rest) -> Rest.
 
-words() ->
-    ok.
-
+%% Interface %%
+words(Bin) ->
+    words(del_space(Bin),<<>>,[]).
+%%
+%% When no more raw binary (<<>>), in Acc last word, %%
+%% in List all other words.                          %%
+%%
+words(<<>>,Acc, List) ->
+    List++[<<Acc/binary>>];
+%%
+%% When we meet space ($\s), send rest binary to     %%
+%% function, nullify Accumulator, Append Acc         %%
+%% (Word before space) to list.                      %%
+%%
+words(<<$\s, Rest/binary>>, Acc,List) ->
+    words(Rest, <<>> ,List++[<<Acc/binary>>]);
+%%
+%% Grab words here, create word (<<Acc>>) from symbols %%
+%% that precede before delimiter.                      %%
+%%
+words(<<X, Bin/binary>>,Acc,List) ->
+    words(Bin, <<Acc/binary, X>>,List).
+    
 %%     Test for words() function    %%
 words_test() ->
     ?assertEqual([<<"Text">>, <<"with">>, <<"four">>, <<"words">>], words(<<"Text with four words">>)),
