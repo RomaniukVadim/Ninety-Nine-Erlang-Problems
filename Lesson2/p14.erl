558d20d5c0ee8145c3702017d1401a10c14f4dfb
Romaniuk Vadim <lavadimka1997@gmail.com>
2018-03-11 02:46:08 +0200

    p14 done. Test passed.

--- Lesson2/p14.erl
+++ Lesson2/p14.erl
@@ -3,7 +3,36 @@
 -export([duplicate/1]).
 -include_lib("eunit/include/eunit.hrl").
 
+%%
 %% (*) Duplicate the elements of a list. %%
 %%
 %% 1> p14:duplicate([a,b,c,c,d]). %%
 %% [a,a,b,b,c,c,c,c,d,d]          %%
+%%
+%%
+%% Reverse func from task p05.erl                  %%
+%% Without reverse output will be in reverse order %%
+reverse(L) ->
+    reverse(L,[]).
+reverse([],Tail) ->
+    Tail;
+reverse([H|T], Tail) ->
+    reverse(T,[H|Tail]).
+%% Reverse func from task p05.erl %%
+%%
+%% Interface                      %%
+duplicate(L) ->
+    duplicate(L,[]).
+%% Receive head, add to DupList two same elements %%
+duplicate([H|T],DupList) ->
+    duplicate(T,[H,H|DupList]);
+%% When raw list is empty, send duplicate List    %%
+duplicate([],L) ->
+    reverse(L).
+
+%%   Test for duplicate() function     %%
+duplicate_test() ->
+    ?assertEqual([a,a,b,b,c,c,c,c,d,d], duplicate([a,b,c,c,d])),
+    ?assertEqual([], duplicate([])),
+    ok.
+%%   Test for duplicate() function     %%
