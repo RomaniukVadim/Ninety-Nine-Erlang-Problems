a3d9ae80f6bde72cb0355a96a594f54421e8462e
Romaniuk Vadim <lavadimka1997@gmail.com>
2018-03-11 20:44:40 +0200

    Comment all matching, all test passed.

--- Lesson2/p10.erl
+++ Lesson2/p10.erl
@@ -15,9 +15,45 @@
 %% 1> p10:encode([a,a,a,a,b,c,c,a,a,d,e,e,e,e]). %%
 %% [{4,a},{1,b},{2,c},{2,a},{1,d},{4,e}]         %%
 %%
-
-encode() ->
-    ok.
+%% len function from p04.erl                     %%
+%%
+len([_H|T]) -> 1+len(T);
+len([]) ->
+    0.
+%%
+%% reverse function from p05.erl                 %%
+%%
+reverse(L) ->
+    reverse(L,[]).
+reverse([],Tail) ->
+    Tail;
+reverse([H|T], Tail) ->
+    reverse(T,[H|Tail]).
+%%
+%% Interface
+encode(L) ->
+    encode(L,[],[]).
+%%
+encode([],[],[]) -> [];
+%% When no list is send to function 
+encode([H|T], [], TiL) ->
+    encode(T,[H],TiL);
+%% Add first element of raw list to list of same elements
+encode([H|T], [H|Rest], TiL) ->
+    encode(T, [H,H|Rest], TiL);
+%% When first element of raw list match first element of
+%% list of same elements, add H to list of same elements.
+encode([H|T], [Hd|Rest],TiL) ->
+    encode(T,[H],[{len([Hd|Rest]),Hd}|TiL]);
+%% When no same elements in raw list, create tuple of length
+%% of same elements in second arg plus one of same element.
+%% Append it to Tuples in List. 
+encode([],[H|T],TiL) ->
+    reverse([{len([H|T]),H}|TiL]).
+%% When raw list is empty display final result.
+%% Aslo in list of same elements we still have some data,
+%% and we need to reverse list, because it displays in
+%% wrong order.
 
 
 %%   Test for encode() function     %%
