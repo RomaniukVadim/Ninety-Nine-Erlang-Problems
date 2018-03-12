287b57d6d413a6e5b478975271ecbfc6545f35a9
Romaniuk Vadim <lavadimka1997@gmail.com>
2018-03-12 17:37:54 +0200

    Remove duplicate p06 from test.erl

--- Lesson2/test.erl
+++ Lesson2/test.erl
@@ -3,7 +3,7 @@
 -export([run/0, run/1]).
 
 run() ->
-    Modules = [p01, p02, p03, p04, p05, p06, p06, p07, p08, p09, p10, p11, p12, p13, p14, p15],
+    Modules = [p01, p02, p03, p04, p05, p06, p07, p08, p09, p10, p11, p12, p13, p14, p15],
     Res = lists:map(fun(M) -> eunit:test(M) end, Modules),
     case Res of
         [ok,ok,ok,ok,ok,ok,ok,ok,ok,ok,ok,ok,ok,ok,ok] -> init:stop(0);
