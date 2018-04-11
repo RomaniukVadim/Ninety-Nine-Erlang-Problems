9fa1b6bd924eda1d3da8d363a2891574b7d4bd5f
glicOne <romaniuk.cv@gmail.com>
2018-04-11 13:35:45 +0300

    Done parse XML

--- Lesson3/bs04.erl
+++ Lesson3/bs04.erl
@@ -1,6 +1,6 @@
 -module(bs04).
 -author('Romaniuk Vadim <romaniuk.cv@gmail.com>').
--export(decode_xml/1).
+-export([decode_xml/1]).
 -include_lib("eunit/include/eunit.hrl").
 
 %%
@@ -14,13 +14,36 @@
 %% {<<"item">>, [], [<<"Text2">>]}
 %% ]}
 %%
+decode_xml(Xml) ->
+	decode_xml(Xml, []). % переходим к двум аргум
 
-decode_xml() ->
-    ok.
+decode_xml(Xml, Word) ->
+	case Xml of
+		<<"</", Rest/binary>> ->
+			bracket_close( Rest, <<>>, Word);
+		<<"<",Rest/binary>> ->
+			bracket_open( Rest, <<>>, Word);
+		<<>> -> []
+	end.
 
-%%     Test for decode_xml() function    %%
-decode_xml_test() ->
-    ?assertEqual({<<"start">>, [], [{<<"item">>, [], [<<"Text1">>]},{<<"item">>, [], [<<"Text2">>]}]} ,decode_xml(<<"<start><item>Text1</item><item>Text2</item></start>">>)),
-    ?assertEqual(<<>>, decode_xml(<<>>)),
-    ok.
-%%     Test for decode_xml() function    %%
+bracket_close(<<">",Rest/binary>>, Acc, [Acc|Word]) ->
+	decode_xml(Rest, Word);
+
+bracket_close(<<X,Rest/binary>>, Acc, Word) ->
+	bracket_close(Rest, <<Acc/binary,X>>, Word).
+
+
+bracket_open(<<"</", Rest/binary>>, Acc, [Bracket|Word]) ->
+	[{Bracket, [], [Acc]} | bracket_close(Rest, <<>>, [Bracket|Word]) ];
+%%
+%%         if "<" write to tuple                                                                        %%
+%%
+bracket_open(<<"<", Rest/binary>>, <<>>, [Bracket|Word]) ->
+	{Bracket, [], decode_xml(<<"<",Rest/binary>>, [Bracket|Word]) }; 
+
+bracket_open(<<">",Rest/binary>>, Acc, Word) ->
+	bracket_open(Rest, <<>>, [Acc|Word]);
+ 
+bracket_open(<<X, Rest/binary>>, Acc, Word) ->
+    bracket_open(Rest, <<Acc/binary,X>>, Word).
+
