5926e6c1f5cafca1be3807a9b2f90293661a16d2
glicOne <romaniuk.cv@gmail.com>
2018-06-03 14:23:36 +0300

    Done p17

--- Lesson2/p17.erl
+++ Lesson2/p17.erl
@@ -3,11 +3,40 @@
 -export([split/2]).
 -include_lib("eunit/include/eunit.hrl").
 
+split(List, FirstPartNum)  when is_integer(FirstPartNum) =:= true ->
+    case FirstPartNum of
+	0 ->
+	    List;
+	_ ->
+	    case List of
+		[] -> [];
+		_ ->
+		    split(List, FirstPartNum, [])
+	    end
+    end.
+
+split([H|T], FirstPartNum, FirstPartList) ->
+    case FirstPartNum of
+	1 ->
+	    [reverse([H|FirstPartList]),T];
+	_ ->
+	    split(T, FirstPartNum - 1, [H|FirstPartList])
+    end.
+
+%%%
+reverse(L) ->
+    reverse(L,[]).
+reverse([],Tail) ->
+    Tail;
+reverse([H|T], Tail) ->
+    reverse(T,[H|Tail]).
+%%%
+
 
 %% Test for split() function   %%
 split_test() ->
     ?assertEqual([[a,b,c], [d,e,f,g,h,i]], split([a,b,c,d,e,f,g,h,i], 3)),
     ?assertEqual([], split([], 3)),
-    ?assertEqual([], split([a,b,c], 0)),
+    ?assertEqual([a,b,c], split([a,b,c], 0)),
     ok.
 %% Test for split() function   %%
