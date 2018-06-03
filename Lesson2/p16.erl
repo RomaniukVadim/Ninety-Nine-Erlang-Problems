229f0b73e452a74af272ba9d9a3f660b425b4c53
glicOne <romaniuk.cv@gmail.com>
2018-06-03 14:04:55 +0300

    Done p16

--- Lesson2/p16.erl
+++ Lesson2/p16.erl
@@ -2,7 +2,36 @@
 -author('Romaniuk Vadim <romaniuk.cv@gmail.com>').
 -export([drop/2]).
 -include_lib("eunit/include/eunit.hrl").
+%% Example:
+%% 1> p16:drop([a,b,c,d,e,f,g,h,i,k], 3).
+%% [a,b,d,e,g,h,k]
 
+drop(List, DropElement) when is_integer(DropElement) =:= true ->
+    case DropElement of
+	0 ->
+	    List;
+	_ ->
+	    drop(List, DropElement, DropElement, [])
+    end.
+
+drop([_|T], DropElement, 1, Result) ->
+    drop(T, DropElement, DropElement, Result);
+
+drop([H|T], DropElement, NumberOfIteration, Result) ->
+    drop(T, DropElement, NumberOfIteration -1, [H|Result]);
+
+drop([],_,_, Result) ->
+    reverse(Result).
+
+
+%%%
+reverse(L) ->
+    reverse(L,[]).
+reverse([],Tail) ->
+    Tail;
+reverse([H|T], Tail) ->
+    reverse(T,[H|Tail]).
+%%%
 
 %% Test for drop() function   %%
 drop_test() ->
