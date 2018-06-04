a49760ee5bb5dfd5949590cf9b18fc737b8a7ae4
glicOne <romaniuk.cv@gmail.com>
2018-06-04 13:38:27 +0300

    Done p33

--- Lesson2/p33.erl
+++ Lesson2/p33.erl
@@ -3,9 +3,31 @@
 -export([coprime/2]).
 -include_lib("eunit/include/eunit.hrl").
 
+coprime(First, Second) when First > 0, Second > 0 ->
+    case gcd(First, Second) == 1 of 
+	true ->
+	    true;
+	_ ->
+	    false
+    end;
+coprime(_First,_Second) ->
+    error.
+
+
+%% greatest common divisor
+gcd(First, Second) when First >= Second->
+    case (First rem Second) >= 1 of
+	true ->
+	    gcd(Second, First rem Second);
+	false ->
+	    Second
+    end;
+gcd(First, Second) ->
+    gcd(Second, First).
 
 %% Test for coprime() function   %%
 coprime_test() ->
-    ?assertEqual(T, coprime(35,64)),
+    ?assertEqual(true, coprime(35,64)),
+    ?assertEqual(false, coprime(35,65)),
     ok.
 %% Test for coprime() function   %%
