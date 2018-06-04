be74950201d5cad98c0b33fa68473b66ec645b07
glicOne <romaniuk.cv@gmail.com>
2018-06-04 13:30:11 +0300

    Done p31

--- Lesson2/p31.erl
+++ Lesson2/p31.erl
@@ -3,9 +3,28 @@
 -export([is_prime/1]).
 -include_lib("eunit/include/eunit.hrl").
 
+is_prime(0)-> false;
+is_prime(1)-> false;
+is_prime(2)-> true;
+
+is_prime(Number) ->
+    case (Number rem 2) =:= 0 of
+        true -> false;
+        _ -> true
+    end.    
 
 %% Test for is_prime() function   %%
 is_prime_test() ->
-    ?assertEqual(T, is_prime(7)),
+    ?assertEqual(false, is_prime(8)),
+    ?assertEqual(true, is_prime(7)),
+    ?assertEqual(true, is_prime(104729)),
+    ?assertEqual(false, is_prime(104730)),
+    ?assertEqual(true, is_prime(101141)),
+    ?assertEqual(true, is_prime(101107)),
+    ?assertEqual(true, is_prime(95393)),
+    ?assertEqual(true, is_prime(86293)),
+    ?assertEqual(true, is_prime(83719)),
+    ?assertEqual(true, is_prime(80747)),
+    ?assertEqual(true, is_prime(78467)),
     ok.
 %% Test for is_prime() function   %%
