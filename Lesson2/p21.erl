3281f35c291dba2f8d3df3cc008c467da8504bfe
glicOne <romaniuk.cv@gmail.com>
2018-06-04 11:29:41 +0300

    Done p21

--- Lesson2/p21.erl
+++ Lesson2/p21.erl
@@ -1,13 +1,48 @@
 -module(p21).
 -author('Romaniuk Vadim <romaniuk.cv@gmail.com>').
--export([insert_at/2]).
+-export([insert_at/3]).
 -include_lib("eunit/include/eunit.hrl").
 
+insert_at(List, Element, Position) when is_integer(Position) =:= true ->
+    case List of
+	[] ->
+	    [Element];
+	_ ->
+	    insert_at(List, Element, Position, [])
+    end.
+
+insert_at(List,Element,0,Result) ->
+    case List of
+	[] ->
+	    reverse(Result);
+	[H|[]] ->
+	    insert_at([],Element,0,[H|Result]);
+	[H|T] ->
+	    insert_at(T,Element,0,[H|Result])
+    end;
+
+
+insert_at([H|T], Element, Position, Result) ->
+    case Position of
+	1 ->
+	    insert_at([H|T], Element, Position-1, [Element|Result]);
+	_ when Position > 0 ->
+	    insert_at(T, Element, Position-1, [H|Result])
+    end.
+
+
+%%%
+reverse(L) ->
+    reverse(L,[]).
+reverse([],Tail) ->
+    Tail;
+reverse([H|T], Tail) ->
+    reverse(T,[H|Tail]).
+%%%
 
 %% Test for insert_at() function   %%
 insert_at_test() ->
-    ?assertEqual([d,e,f,g,h,a,b,c], insert_at([a,b,c,d], alfa, 2)),
-    ?assertEqual([g,h,a,b,c,d,e,f], insert_at([a,b,c,d,e,f,g,h], -2)),
-    ?assertEqual([], insert_at([], 2)),
+    ?assertEqual([a,alfa,b,c,d], insert_at([a,b,c,d], alfa, 2)),
+    ?assertEqual([alfa], insert_at([],alfa, 2)),
     ok.
 %% Test for insert_at() function   %%
