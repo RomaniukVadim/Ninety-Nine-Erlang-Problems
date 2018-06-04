6bb4d489b9d79ce7ca6933fc9ed13bc47947dda7
glicOne <romaniuk.cv@gmail.com>
2018-06-04 10:51:25 +0300

    Done p20

--- Lesson2/p20.erl
+++ Lesson2/p20.erl
@@ -3,6 +3,45 @@
 -export([remove_at/2]).
 -include_lib("eunit/include/eunit.hrl").
 
+remove_at(List, Number) when is_integer(Number) =:= true ->
+    case List of
+	[] -> [];
+	_ ->
+	    case Number of
+		0 ->
+		    List;
+		_ ->
+		    remove_at(List, Number ,[])
+	    end
+    end.
+
+remove_at(End,0,Result) ->
+    case End of
+	[] ->
+	    reverse(Result);
+	[H|[]] ->
+	    remove_at([],0,[H|Result]);
+	[H|T] ->
+	    remove_at(T,0,[H|Result])
+    end;
+
+remove_at([H|T],Number,Result) ->
+    case Number of
+	1 ->
+	    remove_at(T,0,Result);
+	_ ->
+	    remove_at(T,Number-1,[H|Result])  
+    end.
+
+
+%%%
+reverse(L) ->
+    reverse(L,[]).
+reverse([],Tail) ->
+    Tail;
+reverse([H|T], Tail) ->
+    reverse(T,[H|Tail]).
+%%%
 
 %% Test for remove_at() function   %%
 remove_at_test() ->
