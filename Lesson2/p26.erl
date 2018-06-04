7b60e1741e325d29d5dfcffe9491d3f80b03c6aa
glicOne <romaniuk.cv@gmail.com>
2018-06-04 15:10:25 +0300

    Done p26

--- Lesson2/p26.erl
+++ Lesson2/p26.erl
@@ -1,11 +1,18 @@
 -module(p26).
 -author('Romaniuk Vadim <romaniuk.cv@gmail.com>').
--export([combination/2]).
+-export([combination/1]).
 -include_lib("eunit/include/eunit.hrl").
 
+combination([H|T]) ->
+    combination(H,T,[]).
+combination(_C,[],Acc)->
+    Acc;
+combination(C,L,Acc)->
+    [H|T] = L,
+    Acc1 = [[C,X] || X <- L],
+    combination(H,T,Acc1 ++ Acc).
 
 %% Test for combination() function   %%
 combination_test() ->
-    ?assertEqual([[a,b,c], [a,b,d], [a,b,e] ... ], combination([a,b,c,d,e,f],3)),
-    ok.
+    ?assertEqual([[e,f],[d,e],[d,f],[c,d],[c,e],[c,f],[b,c],[b,d],[b,e],[b,f],[a,b],[a,c],[a,d],[a,e],[a,f]], combination([a,b,c,d,e,f])),   ok.
 %% Test for combination() function   %%
