d336a1f07ff9309eca9b935259239d4f992c95f3
glicOne <romaniuk.cv@gmail.com>
2018-06-04 15:04:12 +0300

    Done p28

--- Lesson2/p28.erl
+++ Lesson2/p28.erl
@@ -3,9 +3,20 @@
 -export([lsort/1]).
 -include_lib("eunit/include/eunit.hrl").
 
+lsort([])->
+    [];
+lsort([H|T]) ->
+    lsort(H,T,[],[]).
+
+lsort(P,[],Left,Right) ->
+    lsort(Left) ++ [P] ++ lsort(Right);
+lsort(P,[H|T],Left,Right) when length(H) >= length(P) ->
+    lsort(P,T,Left,[H|Right]);
+lsort(P,[H|T],Left,Right) ->
+    lsort(P,T,[H|Left],Right).
 
 %% Test for lsort() function   %%
 lsort_test() ->
-    ?assertEqual([[o], [d,e], [d,e], [m,n], [a,b,c], [f,g,h], [i,j,k,l]], lsort([[a b c], [d ,e], [f,g,h], [d,e],[i,j,k,l], [m,n], [o]]).,
+    ?assertEqual([[o],[d,e],[m,n],[d,e],[a,b,c],[f,g,h],[i,j,k,l]], lsort([[a,b,c],[d,e],[f,g,h],[d,e],[i,j,k,l],[m,n],[o]])),
     ok.
 %% Test for lsort() function   %%
